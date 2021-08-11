a103e3910e84d0cb99f9f558c4cddfca647f8e85
Thomas Böhm <thomas.boehm@hensoldt-cyber.de>
2021-08-11 10:31:01 +0200

    [DEV] SEOS-3025 Add ARRAY_ELEMENTS macro

---
+++ include/lib_macros/Array.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (C) 2021, HENSOLDT Cyber GmbH
+ */
+
+/**
+ * @file
+ *
+ * Macros to perform operations on arrays.
+ *
+ */
+
+#pragma once
+
+#define ARRAY_ELEMENTS(a) (sizeof(a) / sizeof(a[0]))
