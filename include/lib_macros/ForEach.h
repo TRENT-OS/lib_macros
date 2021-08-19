d6ee88a2d94c54662d0fdd7849456dbd44351a61
Thomas Böhm <thomas.boehm@hensoldt-cyber.de>
2021-08-19 13:06:25 +0200

    [DEV] SEOS-2679 Merge ForEach.h with ChanMuxHelper macros

--- include/lib_macros/ForEach.h
+++ include/lib_macros/ForEach.h
@@ -458,6 +458,263 @@
 // -----------------------------------------------------------------------------
 
 /*
+ * The following are just helper macros, to do a FOR_EACH_nFP_nVP_ loop with n
+ * fixed parameters and n variable ones.
+ */
+
+#define HELPER_FOR_EACH_nFP_nVP_GET_MACRO(_nfp_, _nvp_, _name_) \
+            HELPER_FOR_EACH_ ## _nfp_ ## FP_ ## _nvp_ ## VP_ ##  _name_
+
+// All _dxpy_ and the variable args are thrown away.
+#define HELPER2_FOR_EACH_nFP_2VP_GET_MACRO( \
+            _nfp_, \
+            _d0p0_, _d0p1_, \
+            _d1p0_, _d1p1_, \
+            _d2p0_, _d2p1_, \
+            _d3p0_, _d3p1_, \
+            _d4p0_, _d4p1_, \
+            _d5p0_, _d5p1_, \
+            _d6p0_, _d6p1_, \
+            _d7p0_, _d7p1_, \
+            _name_, \
+            ... \
+        ) \
+            HELPER_FOR_EACH_nFP_nVP_GET_MACRO(_nfp_, 2, _name_)
+
+#define HELPER_FOR_EACH_nFP_2VP_GET_MACRO(_nfp_, ...) \
+            HELPER2_FOR_EACH_nFP_2VP_GET_MACRO( \
+                _nfp_, \
+                __VA_ARGS__, \
+                8,8x,7,7x,6,6x,5,5x,4,4x,3,3x,2,2x,1,1x)
+
+
+#define HELPER_FOR_EACH_0FP_2VP_1(_fn_,_vp0_,_vp1_) \
+            _fn_(_vp0_,_vp1_)
+
+#define HELPER_FOR_EACH_0FP_2VP_2(_fn_,_vp0_,_vp1_, ...) \
+            _fn_(_vp0_,_vp1_) \
+            HELPER_FOR_EACH_0FP_2VP_1(_fn_,__VA_ARGS__)
+
+#define HELPER_FOR_EACH_0FP_2VP_3(_fn_,_vp0_,_vp1_, ...) \
+            _fn_(_vp0_,_vp1_) \
+            HELPER_FOR_EACH_0FP_2VP_2(_fn_,__VA_ARGS__)
+
+#define HELPER_FOR_EACH_0FP_2VP_4(_fn_,_vp0_,_vp1_, ...) \
+            _fn_(_vp0_,_vp1_) \
+            HELPER_FOR_EACH_0FP_2VP_3(_fn_,__VA_ARGS__)
+
+#define HELPER_FOR_EACH_0FP_2VP_5(_fn_,_vp0_,_vp1_, ...) \
+            _fn_(_vp0_,_vp1_) \
+            HELPER_FOR_EACH_0FP_2VP_4(_fn_,__VA_ARGS__)
+
+#define HELPER_FOR_EACH_0FP_2VP_6(_fn_,_vp0_,_vp1_, ...) \
+            _fn_(_vp0_,_vp1_) \
+            HELPER_FOR_EACH_0FP_2VP_5(_fn_,__VA_ARGS__)
+
+#define HELPER_FOR_EACH_0FP_2VP_7(_fn_,_vp0_,_vp1_, ...) \
+            _fn_(_vp0_,_vp1_) \
+            HELPER_FOR_EACH_0FP_2VP_6(_fn_,__VA_ARGS__)
+
+#define HELPER_FOR_EACH_0FP_2VP_8(_fn_,_vp0_,_vp1_, ...) \
+            _fn_(_vp0_,_vp1_) \
+            HELPER_FOR_EACH_0FP_2VP_7(_fn_,__VA_ARGS__)
+
+//------------------------------------------------------------------------------
+#define FOR_EACH_0FP_2VP(_fn_,...) \
+            HELPER_FOR_EACH_nFP_2VP_GET_MACRO(0, __VA_ARGS__)(\
+                _fn_,__VA_ARGS__ \
+            )
+
+//------------------------------------------------------------------------------
+#define FOR_EACH_PARAM_DROP_FIRST(_p0_, ...)        __VA_ARGS__
+
+#define FOR_EACH_PARAM_DROP_0(...)                  __VA_ARGS__
+
+#define FOR_EACH_PARAM_DROP_1(...) \
+            FOR_EACH_PARAM_DROP_FIRST(__VA_ARGS__)
+
+#define FOR_EACH_PARAM_DROP_2(...) \
+            FOR_EACH_PARAM_DROP_1(FOR_EACH_PARAM_DROP_FIRST(__VA_ARGS__) )
+
+#define FOR_EACH_PARAM_DROP_3(...) \
+            FOR_EACH_PARAM_DROP_2(FOR_EACH_PARAM_DROP_FIRST(__VA_ARGS__) )
+
+#define FOR_EACH_PARAM_DROP_4(...) \
+            FOR_EACH_PARAM_DROP_3(FOR_EACH_PARAM_DROP_FIRST(__VA_ARGS__) )
+
+#define FOR_EACH_PARAM_DROP_8(...) \
+            FOR_EACH_PARAM_DROP_4(FOR_EACH_PARAM_DROP_4(__VA_ARGS__) )
+
+#define FOR_EACH_PARAM_DROP_16(...) \
+            FOR_EACH_PARAM_DROP_8( FOR_EACH_PARAM_DROP_8(__VA_ARGS__) )
+
+// We can't use this macro above, because the preprocessor will not resolve the
+// recursion then.
+#define FOR_EACH_PARAM_DROP(_n_, ...) \
+            FOR_EACH_PARAM_DROP_ ## _n_(__VA_ARGS__)
+
+//------------------------------------------------------------------------------
+#define FOR_EACH_PARAM_EXTRACT_FIRST(_p0_, ...)      _p0_
+
+#define FOR_EACH_PARAM_EXTRACT_1(...) \
+            FOR_EACH_PARAM_EXTRACT_FIRST(__VA_ARGS__)
+
+#define FOR_EACH_PARAM_EXTRACT_2(...) \
+            FOR_EACH_PARAM_EXTRACT_FIRST(__VA_ARGS__), \
+            FOR_EACH_PARAM_EXTRACT_1( FOR_EACH_PARAM_DROP(1, __VA_ARGS__))
+
+#define FOR_EACH_PARAM_EXTRACT_3(...) \
+            FOR_EACH_PARAM_EXTRACT_FIRST(__VA_ARGS__), \
+            FOR_EACH_PARAM_EXTRACT_2( FOR_EACH_PARAM_DROP(1, __VA_ARGS__))
+
+#define FOR_EACH_PARAM_EXTRACT_4(...) \
+            FOR_EACH_PARAM_EXTRACT_FIRST(__VA_ARGS__), \
+            FOR_EACH_PARAM_EXTRACT_3( FOR_EACH_PARAM_DROP(1, __VA_ARGS__))
+
+// We can't use this macro above, because the preprocessor will not resolve the
+// recursion then.
+#define FOR_EACH_PARAM_EXTRACT(_n_,...) \
+            FOR_EACH_PARAM_EXTRACT_ ## _n_(__VA_ARGS__)
+
+//------------------------------------------------------------------------------
+
+#define GET_MULTIPLE_PARAMS_AT_IDX(_idx_,_cnt_, ...) \
+            FOR_EACH_PARAM_EXTRACT(_cnt_, FOR_EACH_PARAM_DROP(_idx_, __VA_ARGS__) )
+
+#define GET_PARAM_AT_IDX(_idx_, ...) \
+            GET_MULTIPLE_PARAMS_AT_IDX(_idx_, 1, __VA_ARGS__)
+
+
+//------------------------------------------------------------------------------
+
+// Indirection for invoking a macro.
+#define HELPER_FOR_EACH_EXECUTE_MACRO(_fn_, ...)     _fn_(__VA_ARGS__)
+
+// Helper to remove the brackets from the fixes parameter list.
+#define HELPER_FOR_EACH_DUMMY_MACRO(...)    __VA_ARGS__
+
+// For this macro to work, we need a few level of macros that ensure the
+// parameters are resolved as expected.
+#define FOR_EACH_EXECUTE_MACRO_VP(_fn_, _fp_, _nvp_, ...) \
+            HELPER_FOR_EACH_EXECUTE_MACRO( \
+                _fn_, \
+                HELPER_FOR_EACH_DUMMY_MACRO _fp_, \
+                FOR_EACH_PARAM_EXTRACT(_nvp_, __VA_ARGS__) \
+            )
+
+// These functions deal with 1 to 8 groups of variable parameters.
+#define FOR_EACH_FP_nVP_1(_fn_,_fp_, _nvp_, ...) \
+            FOR_EACH_EXECUTE_MACRO_VP(_fn_, _fp_, _nvp_, __VA_ARGS__)
+
+#define FOR_EACH_FP_nVP_2(_fn_,_fp_, _nvp_, ...) \
+            FOR_EACH_EXECUTE_MACRO_VP(_fn_, _fp_, _nvp_, __VA_ARGS__) \
+            FOR_EACH_FP_nVP_1(_fn_,_fp_, _nvp_, FOR_EACH_PARAM_DROP(_nvp_, __VA_ARGS__))
+
+#define FOR_EACH_FP_nVP_3(_fn_,_fp_, _nvp_, ...) \
+            FOR_EACH_EXECUTE_MACRO_VP(_fn_, _fp_, _nvp_, __VA_ARGS__) \
+            FOR_EACH_FP_nVP_2(_fn_,_fp_, _nvp_, FOR_EACH_PARAM_DROP(_nvp_, __VA_ARGS__))
+
+#define FOR_EACH_FP_nVP_4(_fn_,_fp_, _nvp_, ...) \
+            FOR_EACH_EXECUTE_MACRO_VP(_fn_, _fp_, _nvp_, __VA_ARGS__) \
+            FOR_EACH_FP_nVP_3(_fn_,_fp_, _nvp_, FOR_EACH_PARAM_DROP(_nvp_, __VA_ARGS__))
+
+#define FOR_EACH_FP_nVP_5(_fn_,_fp_, _nvp_, ...) \
+            FOR_EACH_EXECUTE_MACRO_VP(_fn_, _fp_, _nvp_, __VA_ARGS__) \
+            FOR_EACH_FP_nVP_4(_fn_,_fp_, _nvp_, FOR_EACH_PARAM_DROP(_nvp_, __VA_ARGS__))
+
+#define FOR_EACH_FP_nVP_6(_fn_,_fp_, _nvp_, ...) \
+            FOR_EACH_EXECUTE_MACRO_VP(_fn_, _fp_, _nvp_, __VA_ARGS__) \
+            FOR_EACH_FP_nVP_5(_fn_,_fp_, _nvp_, FOR_EACH_PARAM_DROP(_nvp_, __VA_ARGS__))
+
+#define FOR_EACH_FP_nVP_7(_fn_,_fp_, _nvp_, ...) \
+            FOR_EACH_EXECUTE_MACRO_VP(_fn_, _fp_, _nvp_, __VA_ARGS__) \
+            FOR_EACH_FP_nVP_6(_fn_,_fp_, _nvp_, FOR_EACH_PARAM_DROP(_nvp_, __VA_ARGS__))
+
+#define FOR_EACH_FP_nVP_8(_fn_,_fp_, _nvp_, ...) \
+            FOR_EACH_EXECUTE_MACRO_VP(_fn_, _fp_, _nvp_, __VA_ARGS__) \
+            FOR_EACH_FP_nVP_7(_fn_,_fp_, _nvp_, FOR_EACH_PARAM_DROP(_nvp_, __VA_ARGS__))
+
+
+// Call FOR_EACH_FP_nVP_[x](...) after some level of indirection that seem to
+// be necessary for macro parameter evaluation to produce what we need.
+#define HELPER_FOR_EACH_nFP_nVP_caller2(_num_, ...) \
+             FOR_EACH_FP_nVP_ ## _num_(__VA_ARGS__)
+
+// Another layer of indirection that seem necessary.
+#define HELPER_FOR_EACH_nFP_nVP_caller(_num_, ...) \
+            HELPER_FOR_EACH_nFP_nVP_caller2(_num_, __VA_ARGS__)
+
+// This uses the macro HELPER_FOR_EACH_GET_MACRO_NUM_[n]VP() which will be
+// defined below for various values of n.
+#define FOR_EACH_nFP_nVP(_fn_, _nfp_, _nvp_, ...) \
+            HELPER_FOR_EACH_nFP_nVP_caller( \
+                HELPER_FOR_EACH_GET_MACRO_NUM_## _nvp_ ## VP( \
+                    FOR_EACH_PARAM_DROP(_nfp_, __VA_ARGS__) \
+                ), \
+                _fn_, \
+                ( FOR_EACH_PARAM_EXTRACT(_nfp_, __VA_ARGS__) ), \
+                _nvp_, \
+                FOR_EACH_PARAM_DROP(_nfp_, __VA_ARGS__) \
+            )
+
+/*
+ * This macro is to be used in variadic client connection macros, where every
+ * client is connected via n fixed parameters which are shared by all clients
+ * and one client-specific parameter.
+ *
+ * This macro requires another macro (passed via _fn_) which has to look like
+ * this:
+ *
+ *      #define DO_SOMETHING_1P(_inst_,_cli_,_p0_,_num_) \
+ *
+ * It can then be used as follows:
+ *
+ *      FOR_EACH_nFP_1VP(DO_SOMETHING_1P,_inst_,_cli_,__VA_ARGS__)
+ *
+ * The variadic list can contain up to 8 arguments.
+ */
+
+#define HELPER_FOR_EACH_GET_MACRO_NUM_1VP(...) \
+            GET_PARAM_AT_IDX( \
+                8, \
+                __VA_ARGS__, \
+                8,7,6,5,4,3,2,1)
+
+//#define FOR_EACH_0FP_1VP(_fn_, ...)  FOR_EACH_nFP_nVP(_fn_, 0, 1, __VA_ARGS__)
+#define FOR_EACH_1FP_1VP(_fn_, ...)  FOR_EACH_nFP_nVP(_fn_, 1, 1, __VA_ARGS__)
+#define FOR_EACH_2FP_1VP(_fn_, ...)  FOR_EACH_nFP_nVP(_fn_, 2, 1, __VA_ARGS__)
+
+
+//------------------------------------------------------------------------------
+/*
+ * This macro is to be used in variadic client connection macros, where every
+ * client is connected via n fixed parameters which are shared by all clients
+ * and two client-specific parameters.
+ *
+ * This macro requires another macro (passed via _fn_) which has to look like
+ * this:
+ *
+ *      #define DO_SOMETHING_2P(_inst_,_cli_,_p0_,_p1_,_num_) \
+ *          ...
+ *
+ * It can then be used as follows:
+ *
+ *      FOR_EACH_nFP_2VP(DO_SOMETHING_2P,_inst_,_cli_,__VA_ARGS__)
+ *
+ * The variadic list can contain up to 16 arguments (= 8 tuples).
+ */
+
+#define HELPER_FOR_EACH_GET_MACRO_NUM_2VP(...) \
+            GET_PARAM_AT_IDX( \
+                16, \
+                __VA_ARGS__, \
+                8,err8,7,err7,6,err6,5,err5,4,err4,3,err3,2,err2,1,err1)
+
+//#define FOR_EACH_0FP_2VP(_fn_, ...)  FOR_EACH_nFP_nVP(_fn_, 0, 2, __VA_ARGS__)
+#define FOR_EACH_1FP_2VP(_fn_, ...)  FOR_EACH_nFP_nVP(_fn_, 1, 2, __VA_ARGS__)
+#define FOR_EACH_2FP_2VP(_fn_, ...)  FOR_EACH_nFP_nVP(_fn_, 2, 2, __VA_ARGS__)
+
+/*
  * This macro is to be used in variadic client connection macros, where every
  * client is connected without specifying how (e.g., rpc endpoint, or signal,
  *  ...) as macro parameter.
